eb72ac9fded6eaa8c2617d906f60032aba1b0486
Diogo <yogaxpto@gmail.com>
2016-11-11 14:04:36 +0000

    css and javascript changes

--- client/script.js
+++ client/script.js
@@ -1,3 +1,10 @@
 /**
  * Created by Diogo on 20/09/2016.
  */
+function set_tab(tab) {
+    document.getElementsByClassName("login").style.display="none";
+    document.getElementsByClassName("mode").style.display="none";
+    document.getElementsByClassName("game").style.display="none";
+    document.getElementsByClassName("highscores").style.display="none";
+    document.getElementsByClassName(tab).style.display="block";
+}
