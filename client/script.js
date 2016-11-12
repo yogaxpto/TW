f85baf5927c6fe78271e4782db6ac094c5c28174
Diogo <yogaxpto@gmail.com>
2016-11-12 15:57:37 +0000

    register and login implementation on js.

--- client/script.js
+++ client/script.js
@@ -8,3 +8,18 @@ function set_tab(tab) {
     document.getElementsByClassName("highscores").style.display="none";
     document.getElementsByClassName(tab).style.display="block";
 }
+
+function register(){
+    //for now, do as the login() function
+    login()
+}
+
+function login(){
+    if(document.getElementById(username).value!="" && document.getElementById(password)!="") {
+        alert("Logged as " + document.getElementsByClassName(username).value);
+        set_tab("mode")
+    }
+    else{
+        alert("Wrong credentials.\nTry again.");
+    }
+}
